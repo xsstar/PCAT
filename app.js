70b0d7f86bf211308e9fe8ce98f29aca82b98492
Havva Polatli <havvaarslann@gmail.com>
2021-10-10 12:07:21 +0300

    Static files uploaded

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-const fn = function () {
-  console.log('function çaşıştı');
-};
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+//MIDDLEWARES
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
+
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portta başlatıldı..`);
+});
